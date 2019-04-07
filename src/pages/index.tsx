f2bd9a48ac026adceee6c7073089f26d8e386416
Justin Wilkerson <JW@justins-mbp.lan>
2019-04-07 00:39:10 -0400

    Updated netlify.toml with build command.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -9,13 +8,8 @@ import About from "../components/about";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-
     <NextEvent />
     <About />
-
-    {/* <div>
-      <Link to="/page-2/">Go to page 2</Link>
-    </div> */}
   </Layout>
 );
 
