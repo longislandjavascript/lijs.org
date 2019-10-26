b5f1c9fa0518fcba9cbf88c6f21f6769cda98979
Justin Wilkerson <JW@Justins-MBP.home>
2019-10-26 14:54:51 -0400

    Total destruction of old site.

--- src/components/common/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './button';
-export { default as Input } from './input';
-export { default as Select } from './select';
-export { default as Textarea } from './textarea';
-export { default as Section } from './section';
