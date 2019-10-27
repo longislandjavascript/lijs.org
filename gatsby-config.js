be56f97eff7675399dac2434460fa69f8e6e5599
Scott DeVito <scottadevito@gmail.com>
2019-10-27 18:56:45 -0400

    Fixed image/icon.png not found error

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: "src/images/icon.png",
+        icon: "static/icons/icon-196x196.png",
         icons: [
           // {
           //   src: "icons/icon_512x512.png",
