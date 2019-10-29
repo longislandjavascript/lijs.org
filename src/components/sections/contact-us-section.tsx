286355bc7bb02a1d8511209565b72eabbfccd4c3
Scott DeVito <scottadevito@gmail.com>
2019-10-29 00:30:26 -0400

    Create ContactUsSection

---
+++ src/components/sections/contact-us-section.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+import { Section, SectionTitle, CTAButton } from "../common";
+
+export interface ContactUsSectionProps {}
+
+const ContactUsSection: React.FC<ContactUsSectionProps> = () => {
+  return (
+    <Section bg="#fff">
+      <SectionTitle color="#000">Contact Us</SectionTitle>
+      <ContactForm>
+        <InputWrapper>
+          <InputLabel>Name</InputLabel>
+          <Input />
+          <InputLabel>Email</InputLabel>
+          <Input />
+          <InputLabel>Message</InputLabel>
+          <TextArea rows="5" />
+        </InputWrapper>
+        <CTAButton>Submit</CTAButton>
+      </ContactForm>
+    </Section>
+  );
+};
+
+export default ContactUsSection;
+
+const ContactForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 40%;
+  max-width: 60%;
+
+  @media (${props => props.theme.sm}) {
+    min-width: 90%;
+  }
+`;
+
+const InputLabel = styled.span`
+  font-size: 36px;
+  font-weight: 600;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 46px;
+  border-radius: 10px;
+  border: 1px solid #9f9f9f;
+  outline-color: ${props => props.theme.blue};
+  text-indent: 6px;
+  margin: 0 0 30px 0;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 46px;
+  border-radius: 10px;
+  border: 1px solid #9f9f9f;
+  outline-color: ${props => props.theme.blue};
+  text-indent: 6px;
+  margin: 0 0 30px 0;
+`;
+
+const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
