f9e0d0d215d4ef894857dcf442e629249b77002a
Scott DeVito <scottadevito@gmail.com>
2019-10-28 22:55:37 -0400

    Fix section transition svg vertical  spacing gap

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from "../../theme";
+import styled from "styled-components";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyles />
-        <main>{children}</main>
+        <Main>{children}</Main>
       </>
     </ThemeProvider>
   );
 };
 
 export { Layout };
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
